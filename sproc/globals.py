42a504d21c0d576b37447e8adfbee5eac7de6535
Deren <de2356@columbia.edu>
2021-03-31 17:45:31 -0400

    reliable internal data loader

--- sproc/globals.py
+++ sproc/globals.py
@@ -4,9 +4,16 @@
 Global variables.
 """
 
+import os
 import geopandas
 import shapely
 
 # global land cover used to subtract water from convex hull range sizes
-_lands = geopandas.read_file("../geojson/land-cover.json")
-LAND = shapely.geometry.MultiPolygon(_lands.geometry.tolist())
+LANDCOVER_FILE = os.path.join(
+	os.path.dirname(os.path.dirname(__file__)), 
+	"geojson",
+	"land-cover.json",
+)
+LAND = shapely.geometry.MultiPolygon(
+	geopandas.read_file(LANDCOVER_FILE).geometry.tolist()
+)
